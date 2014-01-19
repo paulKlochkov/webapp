fbeeabbb627567099ea79799fec9586211f7cfda
Pavel Klochkov <pavel.klochkow@gmail.com>
2014-01-19 21:33:20 +0200

    init vers

---
+++ out/artifacts/cloud_webapp_war_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: pklochkov
+  Date: 19.01.14
+  Time: 18:18
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Cloud app</title>
+</head>
+<body>
+       PKlochkov cloud app!
+</body>
+</html>
---
+++ src/main/java/by/pklochkov/cloudapp/DispatcherServlet.java
@@ -0,0 +1,21 @@
+package by.pklochkov.cloudapp;
+
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pklochkov
+ * Date: 19.01.14
+ * Time: 18:16
+ * To change this template use File | Settings | File Templates.
+ */
+@javax.servlet.annotation.WebServlet(name = "DispatcherServlet")
+public class DispatcherServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+}
